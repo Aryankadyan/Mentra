d01dd9e6ca1c9bbacb1acacef98107ad881f768b
jai-max <kadyanaryan744@gmail.com>
2025-06-14 23:14:27 +0530

    Set up complete

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home(){
+  return( 
+  <div>Subscribe to me
+  <br/>
+  <Button className="bg-black text-white px-4 py-2 rounded">Click Me</Button>
+     </div>
+  );
+}
---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
+
+const Header = () => {
+  return (
+    <div>
+      <SignedOut />
+      <SignInButton/>
+      <SignedOut/>
+      <SignedIn/>
+      <UserButton/>
+      <SignedIn/>
+    </div>
+  )
+}
+
+export default Header;
