ba06e1f540ab55e007aff8b167b7d2e4a8da60f8
jai-max <kadyanaryan744@gmail.com>
2025-07-18 00:03:45 +0530

    Generation AI Industry Insights

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react'
+import { BarLoader } from 'react-spinners'
 
 const Layout = ({children}) => {
   return (
@@ -6,7 +7,9 @@ const Layout = ({children}) => {
         <div className='flex items-center justify-between mb-5'>
             <h1 className='text-6xl font-bold gradient-title'>Industry Insights</h1>
         </div>
-      <Suspense fallback={<BarLoader className="mt-4 width={"100%"} color="gray" />}</div>>{children}</Suspense>
+      <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color="gray" />}>
+      {children}
+      </Suspense>
     </div>
   )
 }
