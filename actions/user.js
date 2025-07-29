f94520ed377528ed5696c6ef54646b091721ae49
jai-max <kadyanaryan744@gmail.com>
2025-07-29 09:26:17 +0530

    Mock Interview UI

--- actions/user.js
+++ actions/user.js
@@ -68,9 +68,8 @@ export async function updateUser(data) {
 }
 
 export async function getUserOnboardingStatus() {
-  const { userId } = await auth();
-  if (!userId) throw new Error("Unauthorized");
-
+const { userId } = await auth();
+console.log("Clerk userId:", userId);
 
   const user = await db.user.findUnique({
     where: { clerkUserId: userId },
