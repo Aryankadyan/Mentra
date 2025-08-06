d39a63a1e3e25aede23c7901429d4ec834b2bc30
jai-max <kadyanaryan744@gmail.com>
2025-08-06 23:59:26 +0530

    Resume builder UI

---
+++ app/(main)/resume/components/resume-builder.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+const ResumeBuilder = ({initialContent}) => {
+  return(
+    <div>
+      <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+        <h1 className="font-bold gradient-title text-5xl md:text-6xl">
+        ResumeBu
+        </h1>
+  )
+  }
+export default ResumeBuilder;
