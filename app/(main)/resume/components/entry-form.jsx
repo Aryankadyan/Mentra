29b93643d13c70bfc7a548ec1015f1562c91e595
jai-max <kadyanaryan744@gmail.com>
2025-08-12 00:36:06 +0530

    Resume Builder UI

---
+++ app/(main)/resume/components/entry-form.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from "react"
+import { useForm } from 'react-hook-form';
+
+const EntryForm = ({ type, entries, onChange}) => {
+   const {
+    register,
+    handleSubmit: handleValidation,
+    formState: { errors },
+    reset,
+    watch,
+    setValue,
+  } = useForm({
+    resolver: zodResolver(entrySchema),
+    defaultValues: {
+      title: "",
+      organization: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+      current: false,
+    },
+  })
+
+  const current = watch("current")
+};
+
+return <div>EntryForm</div>
+
+export default EntryForm
